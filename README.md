0e358ab8f6297b46528def9280d37742bb8b4bb1
Plínio Pereira <pliniojr@gmail.com>
2019-02-27 06:40:20 -0300

    Primeira etapa :heavy_check_mark:

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ## License
 
 [![GPL-3.0](https://www.gnu.org/graphics/gplv3-127x51.png)](https://www.gnu.org/licenses/quick-guide-gplv3.html)
+
---
+++ src/Scrap_Download_csv.py
@@ -0,0 +1,90 @@
+import os
+from time import sleep
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def remove_repetidos(lista):
+    lista_sem_repetidos = []
+    for i in lista:
+        if i not in lista_sem_repetidos:
+            lista_sem_repetidos.append(i)
+    lista_sem_repetidos.sort()
+    return lista_sem_repetidos
+
+
+def find_datasets(url_site):
+    """Funcao que procura links de possiveis locais para futuro scraping"""
+
+    try:
+        res = requests.get(url_site)
+        res.raise_for_status()
+        soup = bs(res.text, 'html.parser')
+        datasets_links = []
+
+        for a in soup.find_all('a', href=True):
+            if "dataset/" in a['href']:
+                datasets_links.append(a['href'])
+        datasets_links = remove_repetidos(datasets_links)
+
+        sleep(1)
+
+        return datasets_links
+    except Exception as e:
+        print("Failed!\n{}".format(e))
+
+
+def check_and_downloadind_csv(datasets_links, base_url):
+    """Funcao que procura links contendo .csv no final e faz o download para a pasta dataset"""
+    try:
+        for c in datasets_links:
+            url_site = f'{base_url + c}'
+
+            res = requests.get(url_site)
+            res.raise_for_status()
+            # TODO: raise_for_status check
+            soup = bs(res.text, 'html.parser')
+
+            for link in soup.findAll('a', href=True):
+                current_link = link.get("href")
+                if ".csv" in link['href']:
+                    print('Found CSV: ' + current_link)
+                    only_filename = current_link.split('/')
+                    print('Downloading ' + only_filename[-1])
+
+                    sleep(10)
+
+                    response = requests.get(current_link, stream=True)
+
+                    filename = str(only_filename[-1])
+
+                    os.makedirs(str(c[1:] + '/'), exist_ok=True)
+
+                    csvFile = open(os.path.join(str(c[1:] + '/'), os.path.basename(filename)), 'wb')
+                    # TODO: Colocar datas no arquivo MM DD YYYY
+
+                    for data in response.iter_content():
+                        csvFile.write(data)
+                    csvFile.close()
+            sleep(1)
+
+    except Exception as e:
+        print("Failed2!\n{}".format(e))
+
+
+def main():
+    """Função principal da aplicação."""
+
+    base_url = 'http://dados.gov.br'
+    url_site = f'{base_url}/dataset?q=licita%C3%A7%C3%B5es&sort=metadata_modified+desc'
+    os.makedirs('dataset', exist_ok=True)
+
+    datasets_found = find_datasets(url_site)
+    check_and_downloadind_csv(datasets_found, base_url)
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
